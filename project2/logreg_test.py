69f6b41c7799264f30f0f453d5679882bd805971
martinekbh <matan@uio.no>
2019-10-29 17:48:02 +0100

    logreg works

--- project2/logreg_test.py
+++ project2/logreg_test.py
@@ -33,6 +33,9 @@ def accuracy_metric(actual, predicted):
 			correct += 1
 	return correct / float(len(actual)) * 100.0
 
+def accuracy(actual, pred):
+    return np.sum( actual == pred )/len(actual)
+
 # Make a prediction with coefficients
 def predict(row, coefficients):
 	yhat = coefficients[0]
@@ -47,7 +50,7 @@ def coefficients_sgd(train, l_rate, n_epoch):
 		for row in train:
 			yhat = predict(row, coef)
 			error = row[-1] - yhat
-			coef[0] = coef[0] + l_rate * error * yhat * (1.0 - yhat)
+			coef[0] = coef[0] + l_rate * error * yhat * (1.0 - yhat) # intercept
 			for i in range(len(row)-1):
 				coef[i + 1] = coef[i + 1] + l_rate * error * yhat * (1.0 - yhat) * row[i]
 	return coef
@@ -107,6 +110,9 @@ minmax = dataset_minmax(dataset)
 normalize_dataset(dataset, minmax)
 train, test, trash, trash = train_test_split(dataset, y, random_state=0)
 y_test = test[:,-1]
+X_test = test[:,:-1]
+X_train = train[:,:-1]
+y_train = train[:,-1]
 
 """
 # Set up training data
@@ -127,12 +133,66 @@ test = temp
 n_folds = 5
 l_rate = 0.1
 n_epoch = 100
-pred = logistic_regression(train, test, l_rate, n_epoch)
-acc = accuracy_metric(y_test, pred)
-print("accuracy score:", acc)
+#pred = logistic_regression(train, test, l_rate, n_epoch)
+#acc = accuracy_metric(y_test, pred)
+#print("accuracy score:", acc)
 
 
-scores = evaluate_algorithm(dataset, logistic_regression, n_folds, l_rate, n_epoch)
-print('Scores: %s' % scores)
-print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+#scores = evaluate_algorithm(dataset, logistic_regression, n_folds, l_rate, n_epoch)
+#print('Scores: %s' % scores)
+#print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+
+def learning_schedule(t, t0=5, t1=50):
+    return t0/(t+t1)
+
+def mSGD(Xtrain, ytrain, n_epochs, n_minibatches=None, seed=1):
+    np.random.seed(seed)
+    n = len(Xtrain)
+
+    if n_minibatches == None: # Default number of minibatches is n
+        n_minibatches = n
+        batch_size = 1
+
+    else:
+        batch_size = int(n / n_minibatches)
+
+    beta = np.random.randn(len(Xtrain[0]), 1) # why random?
+
+    for epoch in range(n_epochs):   # epoch
+
+        j = 1
+        for i in range(n_minibatches):          # minibatches
+            random_index = np.random.randint(n_minibatches)
+
+            xi = Xtrain[random_index * batch_size: random_index*batch_size + batch_size]
+            yi = ytrain[random_index * batch_size: random_index*batch_size + batch_size]
+            yi = yi.reshape((batch_size, 1))
+
+            p = 1/(1 + np.exp(-xi @ beta))
+            
+            if p.shape != yi.shape:         # Test if something is wrong
+                print("\nOOPS")
+                print("xi:", xi.shape)
+                print("beta:", beta.shape)
+                print("index:", random_index)
+                print("x*b:", (xi@beta).shape)
+                print("p:", p.shape)
+                print("yi:", yi.shape)
+                print(p)
+                exit()
+
+            gradient = -xi.T @ (yi - p) 
+            l = learning_schedule(epoch*n_minibatches + i)
+            beta = beta - l * gradient
+
+    return beta
+
+intercep = np.ones((len(X[:,0]),1))
+X_train = np.concatenate((np.ones((len(X_train), 1)), X_train), axis=1)
+X_test = np.concatenate((np.ones((len(X_test), 1)), X_test), axis=1)
+beta = mSGD(X_train, y_train, n_epoch, n_minibatches = 30, seed=1001)
+pred = np.round(1/(1 + np.exp(-X_test@beta))).ravel()
+acc = np.sum( y_test == pred )/len(y_test)
+print(acc)
+
 
