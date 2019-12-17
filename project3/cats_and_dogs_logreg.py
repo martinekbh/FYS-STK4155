81c6091fdcdb5cb6d9d1ec550fc837377f1190f2
afplaian <andrasfp@student.matnat.uio.no>
2019-12-17 14:12:47 +0100

    added cats_and_dogs_logreg.py

--- project3/cats_and_dogs_logreg.py
+++ project3/cats_and_dogs_logreg.py
@@ -1,97 +1,81 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-from own_code import *
-import time
-
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from skimage.color import rgb2gray
-from sklearn.linear_model import LogisticRegression
+import numpy as np
+from tensorflow import keras
+from LogReg import LogReg
+from own_code import *
+
+
+def accuracy(y, pred):
+    y = y.ravel()
+    pred = pred.ravel()
+    return np.sum(y == pred) / len(y)
+
 
 
 def cats_and_dogs_data_for_logreg():
-    _URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
-    path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
-    # This is the path to the data
-    PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
+    cats_dogs = tf.keras.datasets.cats_and_dogs     # Load dataset
+    (x_train, y_train), (x_test, y_test) = cats_and_dogs.load_data()
 
-    # Path to the training and the testing data
-    train_dir = os.path.join(PATH, 'train')
-    validation_dir = os.path.join(PATH, 'validation')
-    folder = "cats_and_dogs"
+    train_shape = x_train.shape
+    test_shape  = x_test.shape
 
-    # Path to the training/test data divided into cats and dogs
-    train_cats_dir = os.path.join(train_dir, 'cats')  # directory with our training cat pictures
-    train_dogs_dir = os.path.join(train_dir, 'dogs')  # directory with our training dog pictures
-    validation_cats_dir = os.path.join(validation_dir, 'cats')  # directory with our validation cat pictures
-    validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # directory with our validation dog pictures
+    x_train = x_train.ravel().reshape(train_shape[0], train_shape[1]*train_shape[2])/np.max(x_train)
+    x_test =  x_test.ravel().reshape(test_shape[0], test_shape[1]*test_shape[2])/np.max(x_test)
 
-    # Collect info about the dataset
-    num_cats_tr = len(os.listdir(train_cats_dir))
-    num_dogs_tr = len(os.listdir(train_dogs_dir))
-    num_cats_val = len(os.listdir(validation_cats_dir))
-    num_dogs_val = len(os.listdir(validation_dogs_dir))
-    total_train = num_cats_tr + num_dogs_tr
-    total_val = num_cats_val + num_dogs_val
+    one_vector = np.ones((len(y_train),1))
+    X_train1 = np.concatenate((one_vector, x_train), axis = 1)
+    one_vector = np.ones((len(y_test),1))
+    X_test1 = np.concatenate((one_vector, x_test), axis = 1)
 
-    # Print info
-    print("DATASET INFO:")
-    print('Total training cat images:', num_cats_tr)
-    print('Total training dog images:', num_dogs_tr)
-    print('Total validation cat images:', num_cats_val)
-    print('Total validation dog images:', num_dogs_val)
-    print("--")
-    print("Total training images:", total_train)
-    print("Total validation images:", total_val)
-    print("_______________________")
-
-    IMG_HEIGHT = 150
-    IMG_WIDTH = 150
-
-    # Scale (normalize) data
-    train_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our training data
-    validation_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our validation data
-
-    # Create generators that generates the images
-    train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
-                                                           directory=train_dir,
-                                                           shuffle=True,
-                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                           color_mode = color_mode,
-                                                           class_mode='binary')
-
-    val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
-                                                            directory=validation_dir,
-                                                            target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                            color_mode = color_mode,
-                                                            class_mode='binary')
-
-    # Plot 5 randomly selected images from the training set
-    #sample_training_images, _ = next(train_data_gen) 
-    #plotImages(sample_training_images[:5], folder="cats_and_dogs_logreg")
+    return X_train1, y_train, X_test1, y_test
 
 
-def LogReg():
-    print("Doing logistic regression on the cats and dogs data...")
-    start_time = time.time()
+def LogReg_analyze_cats_and_dogs(x_train, y_train, x_test, y_test, epochs, batch_size, n_image):
+    n_train = n_image
+    n_test = int(n_train/100)
 
-    # Fit logreg class
-    logreg = LogisticRegression()
-    logreg.fit(x_train, y_train)    # !!!
+    test_acc_list = []
+    train_acc_list = []
+    for i in range(1, epochs):
+        logreg_analyzer = LogReg(x_train[0:n_train], y_train[0:n_train])
+        logreg_analyzer.sgd(n_epochs= i, n_minibatches= batch_size)
+
+        pred_test = logreg_analyzer.predict(x_test[0:n_test])
+        test_acc_list.append(accuracy(y_test[0:n_test], pred_test))
+
+        pred_train = logreg_analyzer.predict(x_train[0:n_train])
+        train_acc_list.append(accuracy(y_train[0:n_train], pred_train))
+        print(train_acc_list[-1])
+
+
+    return test_acc_list, train_acc_list, np.arange(1,epochs)
+
+
+def plotter(xplot, yplot, plot_label_x, plot_label_y, plot_title, save_text, leg, yplot2 = None):
+    plt.title(plot_title)
+    if yplot2 != None:
+        plt.plot(xplot, yplot2)
+    plt.plot(xplot, yplot)
+    plt.xlabel(plot_label_x)
+    plt.ylabel(plot_label_y)
+    plt.title(plot_title)
+    plt.legend(leg)
+    plt.savefig("\Results\ " + save_text)
+    plt.show()
 
 
 
-    # Print resutls
-    print("Results:")
-    print(f"\n     |  Train    |  Test")
-    print("------------------------------------")
-    print(f"Acc  |{train_acc:10.4f} |{test_acc:10.4f}")
-    print(f"Loss |{train_loss:10.4f} |{test_loss:10.4f}\n")
+#print(train[0][0])
 
+#print( train[1][0])
 
-    
+if "__main__" == __name__:
+    x_train, y_train, x_test, y_test = cats_and_dogs_data_for_logreg()
 
+    test_acc_list, train_acc_list, epoch_interval = LogReg_analyze_cats_and_dogs(x_train, y_train, x_test, y_test, 10, 30, 10000)
+    x_label = "epoch"
+    y_label = "accuracy"
+    title = "Epoch vs accuracy logistic regression"
+    save_text = "epoch_vs_accuracy.pdf"
+    leg = ["train", "test"]
+    plotter(epoch_interval, train_acc_list, x_label, y_label, title, save_text, leg, test_acc_list)
